af7cf1c22b08912e482f1e88dc9c06b917682c52
verykenny <16845292+verykenny@users.noreply.github.com>
2023-02-25 12:19:35 -0700

    simple home and about page using react-router-dom

---
+++ src/pages/AboutUs/index.js
@@ -0,0 +1,12 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our website! We are a team of passionate individuals who strive to provide high-quality products and services to our customers. </p>
+      <p>Our mission is to make a positive impact on the world through our work, and we believe that starts with putting our customers first. </p>
+      <p>If you have any questions or feedback, please don't hesitate to contact us. Thank you for visiting!</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to our website!</h1>
+      <p>Thank you for visiting us. We are a team of passionate individuals who strive to provide high-quality products and services to our customers.</p>
+      <p>Please feel free to browse our website and learn more about what we do. If you have any questions or feedback, don't hesitate to contact us.</p>
+    </div>
+  );
+}
+
+export default Home;
